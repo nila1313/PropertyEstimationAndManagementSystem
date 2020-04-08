e53bdfb6409c35a3df835150533fc5ccbd5dd6f2
TakiTazwar <takitazwarparthib@gmail.com>
2020-04-08 20:21:06 +0600

    Login Folder

--- PropertyEstimationAndManagementSystem/GuiForms/Login.Designer.cs
+++ PropertyEstimationAndManagementSystem/GuiForms/Login.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace PropertyEstimationAndManagementSystem
+﻿namespace PropertyEstimationAndManagementSystem.GuiForms
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Login";
         }
 
         #endregion
     }
 }
-
