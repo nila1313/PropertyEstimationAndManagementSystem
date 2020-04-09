91403bdaa7553fa7b369d86c05739a4475de96d9
TakiTazwar <takitazwarparthib@gmail.com>
2020-04-09 22:57:34 +0600

    USER

--- PropertyEstimationAndManagementSystem/Entites/Users.cs
+++ PropertyEstimationAndManagementSystem/Entites/Users.cs
@@ -9,7 +9,7 @@ namespace PropertyEstimationAndManagementSystem.Entites
     class Users
     {
         public int id { get; set; }
-        public string UserName { get; set; };
-        public string UserPassword { get; set; };
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
     }
 }
