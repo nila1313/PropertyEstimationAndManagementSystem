372429ca8accecf7b52614257a998c3f5733da5c
NasreenMonika <62530607+NasreenMonika@users.noreply.github.com>
2020-05-07 23:00:53 +0600

    reporter Area Pie chart

--- PropertyEstimationAndManagementSystem/GuiForms/ReporterGui/PriceAccordingArea.cs
+++ PropertyEstimationAndManagementSystem/GuiForms/ReporterGui/PriceAccordingArea.cs
@@ -23,9 +23,8 @@ namespace PropertyEstimationAndManagementSystem.GuiForms.Consultant
 
         private void PriceAccordingArea_Load(object sender, EventArgs e)
         {
-
             string sql = "select area, round(sum(price) / sum(size), 2) as 'Average Price per Square Feet' from property group by area";
-
+            setChart();
             dataGridArea.DataSource = da.Execute(sql);
         }
 
@@ -38,6 +37,16 @@ namespace PropertyEstimationAndManagementSystem.GuiForms.Consultant
         private void btnBack_Click(object sender, EventArgs e)
         {
             this.Dispose();
+        }
+
+        public void setChart()
+        {
+            DataTable dt=da.Execute("select Area,Count(*) as 'Total' from Property group by Area order by Total desc");
+            chartArea.Titles.Add("Area Highest Price");
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                chartArea.Series["s1"].Points.AddXY("#PERCENT " + dt.Rows[i][0].ToString(), Convert.ToDouble(dt.Rows[i][1]));
+            }
         }
     }
 }
