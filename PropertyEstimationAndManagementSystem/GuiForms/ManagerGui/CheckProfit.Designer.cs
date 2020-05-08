25179126a3bf75bc40b24ce71bcdcd99f58e450a
SabikunnaharPyaasa <55408189+SabikunnaharPyaasa@users.noreply.github.com>
2020-05-08 22:54:28 +0600

    Profit Calculation

--- PropertyEstimationAndManagementSystem/GuiForms/ManagerGui/CheckProfit.Designer.cs
+++ PropertyEstimationAndManagementSystem/GuiForms/ManagerGui/CheckProfit.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.TotalTransaction = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -40,6 +40,10 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.NetProfit = new System.Windows.Forms.Label();
             this.Profit = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotalProfit = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -49,6 +53,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.TotalTransaction);
             this.panel1.Location = new System.Drawing.Point(45, 39);
             this.panel1.Name = "panel1";
@@ -66,6 +71,7 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.lblTotalProfit);
             this.panel2.Controls.Add(this.TotalProfit);
             this.panel2.Location = new System.Drawing.Point(284, 39);
             this.panel2.Name = "panel2";
@@ -83,6 +89,7 @@
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.TotalSalary);
             this.panel3.Location = new System.Drawing.Point(45, 160);
             this.panel3.Name = "panel3";
@@ -100,6 +107,7 @@
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.NetProfit);
             this.panel4.Location = new System.Drawing.Point(284, 160);
             this.panel4.Name = "panel4";
@@ -117,19 +125,55 @@
             // 
             // Profit
             // 
-            chartArea2.Name = "ChartArea1";
-            this.Profit.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.Profit.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.Profit.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Profit.Legends.Add(legend1);
             this.Profit.Location = new System.Drawing.Point(476, 54);
             this.Profit.Name = "Profit";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.Profit.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "s1";
+            this.Profit.Series.Add(series1);
             this.Profit.Size = new System.Drawing.Size(300, 300);
             this.Profit.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Transaction";
+            // 
+            // lblTotalProfit
+            // 
+            this.lblTotalProfit.AutoSize = true;
+            this.lblTotalProfit.Location = new System.Drawing.Point(3, 0);
+            this.lblTotalProfit.Name = "lblTotalProfit";
+            this.lblTotalProfit.Size = new System.Drawing.Size(58, 13);
+            this.lblTotalProfit.TabIndex = 1;
+            this.lblTotalProfit.Text = "Total Profit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Total Salary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Net Profit";
             // 
             // CheckProfit
             // 
@@ -143,6 +187,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "CheckProfit";
             this.Text = "CheckProfit";
+            this.Load += new System.EventHandler(this.CheckProfit_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -167,5 +212,9 @@
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label NetProfit;
         private System.Windows.Forms.DataVisualization.Charting.Chart Profit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotalProfit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- PropertyEstimationAndManagementSystem/GuiForms/ManagerGui/CheckProfit.cs
+++ PropertyEstimationAndManagementSystem/GuiForms/ManagerGui/CheckProfit.cs
@@ -14,7 +14,7 @@ namespace PropertyEstimationAndManagementSystem.GuiForms.ManagerGui
     public partial class CheckProfit : Form
     {
         DataAccess da;
-
+        double totalSale, totalBuy;
         public CheckProfit()
         {
             InitializeComponent();
@@ -23,21 +23,66 @@ namespace PropertyEstimationAndManagementSystem.GuiForms.ManagerGui
         }
         public void setPanel()
         {
+            string thisMonth;
             string prob = @"""Transaction""";
-            string thisWeekSunday = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + (int)DayOfWeek.Sunday).ToString("MM/dd/yyyy");
-            string sql = @"select count(*) from " + prob + $" where convert(varchar, TransactionDateTime,101)>='{thisWeekSunday}'";
+            if(DateTime.Now.Month<10)
+            {
+                thisMonth ="0"+ DateTime.Now.Month.ToString() + @"/01/" + DateTime.Now.Year.ToString();
+            }
+            else
+            {
+                thisMonth = DateTime.Now.Month.ToString() + @"/01/" + DateTime.Now.Year.ToString();
+            }
+            
+            //TOTAL TRANSACTION
+            string sql = $"select count(*) from {prob} where convert(varchar, TransactionDateTime,101)>='{thisMonth}'";
             DataTable dt = da.Execute(sql);
             TotalTransaction.Text = dt.Rows[0][0].ToString();
+            
+            //TOTAL SALARY
+            sql = @"select SUM(Salary) from Employee";
+            dt = da.Execute(sql);
+            TotalSalary.Text= dt.Rows[0][0].ToString();
+
+            //TOTAL PROFIT
+            sql = @"select SUM(amount) from " + prob + $" where Trade='SOLD' and convert(varchar, TransactionDateTime,101)>='{thisMonth}'";
+            dt = da.Execute(sql);
+            this.totalSale = Convert.ToDouble(dt.Rows[0][0]);
+            sql = @"select SUM(amount) from " + prob + $" where Trade='BOUGHT' and convert(varchar, TransactionDateTime,101)>='{thisMonth}'";
+            dt = da.Execute(sql);
+            this.totalBuy = Convert.ToDouble(dt.Rows[0][0]);
+            double totalProfit = totalSale - totalBuy;
+            if (totalProfit<0)
+            {
+                lblTotalProfit.Text = "Total Loss";
+            }
+            TotalProfit.Text = Math.Abs(totalProfit).ToString();
+
+            //TOTAL NET PROFIT
+            NetProfit.Text = (Convert.ToDouble(TotalProfit.Text) - Convert.ToDouble(TotalSalary.Text)).ToString();
+        }
+
+
+        private void CheckProfit_Load(object sender, EventArgs e)
+        {
+            setChart();
         }
 
         public void setChart()
         {
-            Profit.Titles.Add("Profit Estimation");
-            Profit.Series["s1"].Points.AddXY("#PERCENT Transaction", Convert.ToDouble(TotalTransaction.Text));
-            Profit.Series["s1"].Points.AddXY("#PERCENT Profit", Convert.ToDouble(TotalProfit.Text));
-            Profit.Series["s1"].Points.AddXY("#PERCENT Salary", Convert.ToDouble(TotalSalary.Text));
-            Profit.Series["s1"].Points.AddXY("#PERCENT Net Profit", Convert.ToDouble(NetProfit.Text));
+            Profit.Titles.Add("Total Transaction,salary,Profit estimation");
+            //Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(TotalTransaction.Text));
+            //Profit.Series["s1"].Points[0].LegendText = "Total Transaction";
+            //Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(TotalProfit.Text));
+            //Profit.Series["s1"].Points[0].LegendText = "Total Profit";
+            Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(totalSale));
+            Profit.Series["s1"].Points[0].LegendText = "Total Sale";
+            Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(totalBuy));
+            Profit.Series["s1"].Points[1].LegendText = "Total Buy";
+            Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(TotalSalary.Text));
+            Profit.Series["s1"].Points[2].LegendText = "Total Salary";
+            Profit.Series["s1"].Points.AddXY("#PERCENT", Convert.ToDouble(NetProfit.Text));
+            Profit.Series["s1"].Points[3].LegendText = "Net Profit";
         }
-
     }
 }
